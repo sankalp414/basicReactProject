aae0503413cad81aff5ab7a867ad7c7c9f680df6
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-12 18:29:26 +0000

    react

---
+++ react-project-1/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+    <h1>Hello Sankalp I will be the great developer</h1>
+  )
+}
+
+export default App
---
+++ react-project-2/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+     <h1> Hello Sankalp I will be a great developer</h1>    
+  );
+}
+
+export default App;
