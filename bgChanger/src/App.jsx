8c42c395137143eeb3d5c83d58bb75494bd769b8
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-13 18:16:03 +0000

    bgChanger

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const [color, setColor] = useState("red")
+
+  return (
+    <div className= " w-full h-screen duration-200 "
+      style = {{backgroundColor: color}}
+    >
+      
+      
+    </div>
+  )
+}
+
+export default App
