7eba82a99963d57f584cf50d1c9d4c1f3cc99a34
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-16 18:05:11 +0000

    bgChanger

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 function App() {
-  const [color, setColor] = useState("red")
+  const [color, setColor] = useState("red") // use state hook 
 
   return (
     <div className= " w-full h-screen duration-200 "
