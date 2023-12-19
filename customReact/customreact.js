8e22eea190a8e367937b4f32647a99f436798a29
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-19 17:31:31 +0000

    customReact

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,6 +1,6 @@
 function customRender(reactElement,container){
     const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
+    domElement.innerHTML = reactElement.children// react
     domElement.setAttribute('href',reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
