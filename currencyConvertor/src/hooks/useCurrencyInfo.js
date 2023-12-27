30869f85cb501b35ab525a36dd178e37f3b3aa07
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-27 06:39:48 +0000

    currencyConvertor

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import { useEffect,useState } from "react"
+
+function useCurrencyInfo(currency){
+    const [data,useData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=> res.json())
+        .then((res)=>SVGMetadataElement(res[currency]))
+    },[currency])
+    return data
+}
+
+export default useCurrencyInfo;
