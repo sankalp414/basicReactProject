468a8b3f79b607f90484f048df7a8c795bb37e76
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-29 17:07:06 +0000

    basic

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,22 +1,37 @@
 function customRender(reactElement,container){
-    const domElement = document.createElement(reactElement.type)
+/*    const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children// react
     domElement.setAttribute('href',reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
-    container.appendChild(domElement)
+    container.appendChild(domElement)*/
+
+     const domElement = document.createElement(reactElement.type) //this is a better approach then the upper one
+     domElement.innerHTML = reactElement.children
+     for (const prop in reactElement.props) {
+        if (prop === 'children') continue;//it was used to be done in older days
+         {
+            domElement.setAttribute(prop,reactElement.props[prop])
+            
+
+        }
+        container.appendChild(domElement)
+     }
+
 }
 
-const reactElement = {
-    type: 'a',
-    props: {
+
+
+const reactElement = {  //this is how react takes html code after compilation 
+    type: 'a',  //this shows what type of element it is
+    props: { //it shows what type of properties it has
         href: 'https://google.com',
         target: '_blank'
     },
     children : 'Click me to visit google'
-}
+}//we will get every element like this which is written in html in react js 
 
 
-const mainContainer = document.getElementById('root')
-
-customRender(reactElement, mainContainer)
+const mainContainer = document.getElementById('root') //selected the id root from the index.html file so that we can make changes in the file through js
+ 
+customRender(reactElement, mainContainer)//this method renders the object of the html code
--- react-project-1/src/main.jsx
+++ react-project-1/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App \ </h1> 
+            
+        </div>
+    )
+}
+const reactElement =  React.createElement (
+    'a',
+    {href:'https://google.com', target:'_blank'},
+    'click me to visit'
+
+
+)
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   
-    <App />
+    <MyApp />
   
 )
