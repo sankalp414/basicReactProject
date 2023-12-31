a467b13004edfd1561691f1a92c137bff2f41ac6
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-31 18:50:16 +0000

    reactRouter

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Layout from './Layout.jsx'
 import Contact from './components/Contact/Contect.jsx'
+import User from './components/User/User.jsx'
 
 
 // const router = createBrowserRouter([
@@ -38,6 +39,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />}/>
       <Route path='about' element={<About />}/>
       <Route path='contact' element={<Contact />}/>
+      <Route path='user/:userid' element={<User />}/>
     </Route>
   )
 )
